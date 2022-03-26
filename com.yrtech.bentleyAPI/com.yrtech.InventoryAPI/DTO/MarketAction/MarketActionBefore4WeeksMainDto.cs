63f8c056abb781f1b5fc7914f4ebb75a8187bb79
Tony <moujunsheng15@163.com>
2022-03-26 10:12:38 +0800

    fff

---
+++ com.yrtech.bentleyAPI/com.yrtech.InventoryAPI/DTO/MarketAction/MarketActionBefore4WeeksMainDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using com.yrtech.bentley.DAL;
+
+namespace com.yrtech.InventoryAPI.DTO
+{
+    [Serializable]
+    public class MarketActionBefore4WeeksMainDto
+    {
+        public int MarketActionId { get; set; }
+        public string TarketModelCode { get; set; }// 主推车型
+        public MarketActionBefore4Weeks MarketActionBefore4Weeks { get; set; } // 活动计划信息
+        public List<MarketActionBefore4WeeksCoopFund> MarketActionBefore4WeeksCoopFund { get; set; } // 市场基金申报信息
+        public List<MarketActionBefore4WeeksActivityProcess> ActivityProcess { get; set; } // 活动流程信息
+        public List<MarketActionPic> MarketActionBefore4WeeksPicList_OffLine { get; set; } // 线下照片
+        public List<MarketActionPic> MarketActionBefore4WeeksPicList_OnLine { get; set; }// 线上照片
+
+    }
+}
